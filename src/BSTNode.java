a1306a3b73df1aa692c09ad30f7254597359bed2
markzhu22 <mzhu22@vt.edu>
2024-09-25 13:29:01 -0400

    Update BSTNode.java

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -1,6 +1,73 @@
 public class BSTNode
 {
-    public BSTNode() {
-        
+    private Seminar element; // Element for this node
+    private BSTNode left;          // Pointer to left child
+    private BSTNode right;         // Pointer to right child
+
+    // Constructors
+    public BSTNode()
+    {
+        left = right = null;
+    }
+
+
+    public BSTNode(Seminar val)
+    {
+        left = right = null;
+        element = val;
+    }
+
+
+    public BSTNode(Seminar val, BSTNode l, BSTNode r)
+    {
+        left = l;
+        right = r;
+        element = val;
+    }
+
+
+    // Get and set the element value
+    public Seminar value()
+    {
+        return element;
+    }
+
+
+    public void setValue(Seminar v)
+    {
+        element = v;
+    }
+
+
+    // Get and set the left child
+    public BSTNode left()
+    {
+        return left;
+    }
+
+
+    public void setLeft(BSTNode p)
+    {
+        left = p;
+    }
+
+
+    // Get and set the right child
+    public BSTNode right()
+    {
+        return right;
+    }
+
+
+    public void setRight(BSTNode p)
+    {
+        right = p;
+    }
+
+
+    // return TRUE if a leaf node, FALSE otherwise
+    public boolean isLeaf()
+    {
+        return (left == null) && (right == null);
     }
 }
